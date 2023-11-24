b85cf560439c13cba88bd3c1ea76f59c754fd612
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2023-11-24 09:25:28 +0100

    Initial Commit

---
+++ AuthentificationWithGoogle/AuthentificationWithGoogleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthentificationWithGoogleApp.swift
+//  AuthentificationWithGoogle
+//
+//  Created by Thibault GODEFROY on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthentificationWithGoogleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
